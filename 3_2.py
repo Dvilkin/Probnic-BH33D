d38ed94fc382ae63507cef5c22f0836d0b253862
Dvilkin <Dvilkin@bk.ru>
2022-08-17 02:33:03 +0300

    add 3_2 ok

--- 3_2.py
+++ 3_2.py
@@ -1,2 +1,7 @@
 # Пользователь вводит 3 числа, найти среднее арифмитическое с точность 3
 
+chislo_1: float = float(input('Введите число 1:'))
+chislo_2: float = float(input('Введите число 2:'))
+chislo_3: float = float(input('Введите число 3:'))
+sr_arifmeticheckoe = float((chislo_1+chislo_2+chislo_3)/3)
+print('Среднее арифмитическое =', round(sr_arifmeticheckoe, 3))
