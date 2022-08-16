9cf89984bda4f334e2ccce2bf94cf5bf9d640da0
Dvilkin <Dvilkin@bk.ru>
2022-08-17 01:16:04 +0300

    zadacha 3_1 ok

--- 3_1.py
+++ 3_1.py
@@ -1,5 +1,12 @@
 # Пользователь вводит предложение, заменить все пробелы на "-" 2-мя способами
 
+#способ №1
 stroka = input('Введите сторку 1:')
 stroka = stroka.replace(' ', '-')
+print(stroka)
+
+#способ №2
+stroka = input('Введите сторку 2:')
+stroka = stroka.split()
+stroka = '-'.join(stroka)
 print(stroka)
