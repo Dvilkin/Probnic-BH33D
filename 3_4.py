fb1e8f7fd0a353e12eb7ed246bf83bddfa7b495a
Dvilkin <Dvilkin@bk.ru>
2022-08-17 04:06:04 +0300

    add 3_4 ok

--- 3_4.py
+++ 3_4.py
@@ -1,1 +1,9 @@
 #Пользователь вводит 3 числа, сказать сколько из них положительных и сколько отрицательных
+
+x_1: float = float(input('Введите число 1:'))
+x_2: float = float(input('Введите число 2:'))
+x_3: float = float(input('Введите число 3:'))
+polozitelnix = (x_1>0) + (x_2>0) + (x_3>0)
+otricatelnix = 3 - polozitelnix
+print("Положительных", polozitelnix)
+print("Отрицательных", otricatelnix)
