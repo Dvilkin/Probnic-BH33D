1cb574efe9bef7acc801b74b25d5f4a4eac75b30
Dvilkin <Dvilkin@bk.ru>
2022-08-20 17:57:30 +0300

    sozdan list

--- 4_2.py
+++ 4_2.py
@@ -1,3 +1,11 @@
 # Без использования collections, написать программу, которая будет создавать словарь
 # для подсчитывания количества вхождений каждой буквы в текст введенный с клавиатуры
 
+
+stroka: str = str(input('Введите строку:'))
+dlina = len(stroka)
+print(len(stroka))
+s = stroka[:]
+print(s)
+list = {n: (stroka[n]) for n in range(0, dlina)}
+print(list)
