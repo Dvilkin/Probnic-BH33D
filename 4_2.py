19f97640bd90c4fc41443d9a9c47986cee87ca37
Dvilkin <Dvilkin@bk.ru>
2022-08-20 19:40:23 +0300

    19.40

--- 4_2.py
+++ 4_2.py
@@ -1,10 +1,12 @@
 # Без использования collections, написать программу, которая будет создавать словарь
 # для подсчитывания количества вхождений каждой буквы в текст введенный с клавиатуры
 
-
-stroka: str = str(input('Введите строку:'))
+stroka: str = str(input('Введите текст:'))
 dlina = len(stroka)
 print(len(stroka))
 list = {n: (stroka[n]) for n in range(0, dlina)}
 print(list)
-
+list2 = [(stroka[n]) for n in range(0, dlina)]
+print(list2)
+list3 = list2.count((stroka(n)) for n in range(0, dlina))
+print(list3)
