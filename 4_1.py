56eafdc6c1fbd923c7657a1751fca443e321d40d
Dvilkin <Dvilkin@bk.ru>
2022-08-20 03:10:09 +0300

    syka list ne sozdaetsa

--- 4_1.py
+++ 4_1.py
@@ -1,5 +1,11 @@
 # Заполнить список степенями числа 2 (от 2^1 до 2^n)
 
-n: int = int(input ('Введите сепень n:'))
-steneni = [i for i in range(2**1, 2**n)]
-print(steneni)
+n: int = int(input('Введите сепень числа 2:'))
+for i in range(1, n + 1):
+    print(2 ** i)
+
+list = [i for i in range(1, n + 1)]
+print(list)
+
+# for i in range(1,n):
+#     print(2**i)
