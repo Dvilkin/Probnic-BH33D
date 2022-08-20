3b5e6b7321a096459b6659f6c2f84919972fa9a8
Dvilkin <Dvilkin@bk.ru>
2022-08-20 03:43:08 +0300

    4_1 ok

--- 4_1.py
+++ 4_1.py
@@ -1,11 +1,5 @@
 # Заполнить список степенями числа 2 (от 2^1 до 2^n)
 
 n: int = int(input('Введите сепень числа 2:'))
-for i in range(1, n + 1):
-    print(2 ** i)
-
-list = [i for i in range(1, n + 1)]
+list = [2 ** i for i in range(1, n + 1)]
 print(list)
-
-# for i in range(1,n):
-#     print(2**i)
