f9030a6cb15cc2c387449de09d78410377aa233a
Dvilkin <Dvilkin@bk.ru>
2022-08-21 12:53:07 +0300

    4_3 ok, tolko vse bez perenosov

--- 4_3.py
+++ 4_3.py
@@ -3,13 +3,7 @@
 # для этих ключей будут браться с клавиатуры
 
 n: int = int(input('ENTER n:'))
-name = input('ENTER Name:')
-email = input('ENTER email:')
-data = {
-    'NAME': name,
-    'EMAIL': email
-}
 biblioteka = {
-    n: data for n in range(1, n+1)}
+    n: {'NAME': input('ENTER Name:'),
+        'EMAIL': input('ENTER email:')} for n in range(1, n+1)}
 print(biblioteka)
-print(data)
