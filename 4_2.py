327ff533335f8fb641933023b1c582a807ce6c5b
Dvilkin <Dvilkin@bk.ru>
2022-08-21 15:17:03 +0300

    add stroka

--- 4_2.py
+++ 4_2.py
@@ -1,18 +1,26 @@
 # Без использования collections, написать программу, которая будет создавать словарь
 # для подсчитывания количества вхождений каждой буквы в текст введенный с клавиатуры
 
-# dlina = len(stroka)
-# print(len(stroka))
+stroka: str = str(input('Введите текст:'))
+dlina = len(stroka)
+print(len(stroka))
 # list = {n: (stroka[n]) for n in range(0, dlina)}
 # print(list)
 # list2 = [(stroka[n]) for n in range(0, dlina)]
 # print(list2)
-# list3 = list2.count((stroka(n)) for n in range(0, dlina))
-# print(list3)
-stroka: str = str(input('Введите текст:'))
-dlina = len(stroka)
-print(len(stroka))
-kol_bykv = {
-    stroka[0]
+n = int(0)
+print(n)
+list_vse = {
+    stroka[n]: stroka.count(n) for n in range(0, dlina-1)
 }
-print(kol_bykv)
+print(list_vse)
+
+# list3 = list2.count((stroka(n)) for n in range(0, dlina))
+
+#
+# dlina = len(stroka)
+# print(len(stroka))
+# kol_bykv = {
+#     stroka[n]:
+# }
+# print(kol_bykv)
