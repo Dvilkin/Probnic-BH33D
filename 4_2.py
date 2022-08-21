7d5d4ff130cf94a3efed10f6a3f99186a858c2b8
Dvilkin <Dvilkin@bk.ru>
2022-08-21 19:35:22 +0300

    4_2 ok

--- 4_2.py
+++ 4_2.py
@@ -1,26 +1,10 @@
 # Без использования collections, написать программу, которая будет создавать словарь
 # для подсчитывания количества вхождений каждой буквы в текст введенный с клавиатуры
 
-stroka: str = str(input('Введите текст:'))
+stroka: str = str(input('Enter text:'))
 dlina = len(stroka)
-print(len(stroka))
-# list = {n: (stroka[n]) for n in range(0, dlina)}
-# print(list)
-# list2 = [(stroka[n]) for n in range(0, dlina)]
-# print(list2)
-n = int(0)
-print(n)
+print('Kolichestvo vvedenix simvolov:', len(stroka))
 list_vse = {
-    stroka[n]: stroka.count(n) for n in range(0, dlina-1)
+    stroka[n]: stroka.count(stroka[n]) for n in range(0, dlina)
 }
 print(list_vse)
-
-# list3 = list2.count((stroka(n)) for n in range(0, dlina))
-
-#
-# dlina = len(stroka)
-# print(len(stroka))
-# kol_bykv = {
-#     stroka[n]:
-# }
-# print(kol_bykv)
