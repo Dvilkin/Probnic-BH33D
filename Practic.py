82cd2939f58e44c018937d43c897a56ed5502845
Dvilkin <Dvilkin@bk.ru>
2022-08-23 13:31:31 +0300

    zadachi

--- Practic.py
+++ Practic.py
@@ -1,7 +1,22 @@
-#Вводится строка, вывести уникальные символы
+# Вводится строка, вывести уникальные символы
+#
+# stroka = input("Enter stroky:")
+# for x in set(stroka):
+#     if stroka.count(x) == 1:
+#         print(x)
 
-stroka = input("Enter stroky:")
-for x in set(stroka):
-    if stroka.count(x) == 1:
-        print(x)
+# Вводится число, посчитать его факториал
 
+# chislo = int(input("Enter chislo:"))
+# factorial = 1
+# for x in range(1, chislo+1):
+#     factorial *=x
+#     print(factorial)
+
+#Составить программу дял вычисления среднего арифметического N произвольно вводимых чисел
+
+n = int(input("Enter chislo:"))
+s = 0
+for i in range(n):
+    s+= int(input())
+print(s/n)
