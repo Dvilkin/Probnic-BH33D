cffcc26e4641ed8c1f24168f7df42ac6de055822
Dvilkin <Dvilkin@bk.ru>
2022-08-23 22:48:11 +0300

    5_1 ok

--- 5_1.py
+++ 5_1.py
@@ -3,11 +3,12 @@
 n: int = int(input("Enter kolichestvo chisel:"))
 m: int = int(input("Enter kratnost chisel:"))
 k: int = int(input("Enter bolhe chisla:"))
-chisla = 0
-kol_chisel = 0
-while kol_chisel <= n:
-    if k % m == 0:
-        print(k)
-        kol_chisel +=1
-    k += 1
 
+kol_chisel = 0
+while kol_chisel < n:
+    if k % m == 0 and k != 0:
+        print(k)
+        kol_chisel += 1
+        k += m
+    else:
+        k += 1
