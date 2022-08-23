72bcf38e55bfafddea4d89ac4762d77e8902fe00
Dvilkin <Dvilkin@bk.ru>
2022-08-23 23:35:49 +0300

    5_2 ok

--- 5_2.py
+++ 5_2.py
@@ -1,1 +1,19 @@
 # Сделать калькулятор: у пользователя спрашивается число, потом действие и второе число
+
+cislo1: float = float(input("Enter 1-voe chislo:"))
+deistvie: str = input("Enter deistvie (+), (-), (*), (/), (%), (**), (//): ")
+cislo2: float = float(input("Enter 2-voe chislo:"))
+if deistvie == "+":
+    print(cislo1 + cislo2)
+elif deistvie == "-":
+    print(cislo1 - cislo2)
+elif deistvie == "*":
+    print(cislo1 * cislo2)
+elif deistvie == "/":
+    print(cislo1 / cislo2)
+elif deistvie == "%":
+    print(cislo1 % cislo2)
+elif deistvie == "**":
+    print(cislo1 ** cislo2)
+elif deistvie == "//":
+    print(cislo1 // cislo2)
