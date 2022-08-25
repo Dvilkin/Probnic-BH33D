d962b87406ec7ea3c5708aed9ea30678ea9c83f8
Dvilkin <Dvilkin@bk.ru>
2022-08-26 00:37:13 +0300

    5_7 ok

--- 5_7.py
+++ 5_7.py
@@ -1,7 +1,9 @@
 # Дан список чисел, отсортировать его по возрастанию без использования sort и sorted
 
-spisok_cisel = []
-spisok_cisel = (input())
-qwe = tuple[(spisok_cisel)]
+spisok_cisel = list(map(int, input('Enter celie cisla cherez probel:').split()))
 print(spisok_cisel)
-print(tuple(spisok_cisel))
+for all_element in range(len(spisok_cisel) - 1):
+    for i in range(len(spisok_cisel) - 1 - all_element):
+        if spisok_cisel[i] > spisok_cisel[i + 1]:
+            spisok_cisel[i], spisok_cisel[i + 1] = spisok_cisel[i + 1], spisok_cisel[i]
+print(spisok_cisel)
