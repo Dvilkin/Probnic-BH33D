d5247399cfb18cdf40ca533facf4e8734d2c0dd2
Dvilkin <Dvilkin@bk.ru>
2022-08-25 23:06:18 +0300

    initial commit

---
+++ 5_7.py
@@ -0,0 +1,7 @@
+# Дан список чисел, отсортировать его по возрастанию без использования sort и sorted
+
+spisok_cisel = []
+spisok_cisel = (input())
+qwe = tuple[(spisok_cisel)]
+print(spisok_cisel)
+print(tuple(spisok_cisel))
