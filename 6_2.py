d9fde4983b35cb798f0ebea5893d0f1481c1c18f
Dvilkin <Dvilkin@bk.ru>
2022-08-29 00:34:51 +0300

    add slovar morse

--- 6_2.py
+++ 6_2.py
@@ -14,21 +14,26 @@ a = input("Enter text:")
 def Morse(s):
     a = ''
     for i in s.lower():
-        if i in morse.keys(): a += morse[i] + ' '
-        if i == ' ': a += ' / '  # Слова разделяются слэшем
+        if i in morse.keys():
+            a += morse[i] + ' '  # Буквы разделяем пробелом
+        if i == ' ':
+            a += ' / '  # Слова разделяются слэшем
     return a
 
 
 def get_key(val, dct):  # Функция возвращает ключ словаря по значению
     for key, value in dct.items():
-        if val == value: return key
+        if val == value:
+            return key
 
 
 def MorseTxt(s):
     a, s = '', s.split()
     for i in s:
-        if i in morse.values(): a += get_key(i, morse)
-        if i == '/': a += ' '
+        if i in morse.values():
+            a += get_key(i, morse)
+        if i == '/':
+            a += ' '
     return a
 
 
