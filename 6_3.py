1ed116374cf340317c9d59c032816602c6d3ab8a
Dvilkin <Dvilkin@bk.ru>
2022-08-29 23:48:57 +0300

    6_3 ok

---
+++ 6_3.py
@@ -0,0 +1,7 @@
+# Дан список чисел и на вход поступает число N, необходимо сместить список на
+# указанное число, пример: [1,2,3,4,5,6,7] N=3 ответ: [5,6,7,1,2,3,4]
+
+spisik_cisel = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+smescenie: int = int(input("Enter smescenie ot 1 do 8:"))
+spisik_cisel = spisik_cisel[-smescenie:] + spisik_cisel[:-smescenie]
+print(spisik_cisel)
