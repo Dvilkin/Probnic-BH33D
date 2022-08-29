b55cfe43834e21cf7f3036c387b94a7df214f964
Dvilkin <Dvilkin@bk.ru>
2022-08-30 01:11:28 +0300

    add for

--- 6_4.py
+++ 6_4.py
@@ -1,3 +1,12 @@
 # Дан список содержащий в себе различные типы данных, отфильтровать таким
 # образом, чтобы остались только строки, использование дополнительного списка незаконно
 
+spisok = [1, 2, 3, 'Leto', [89, 90, 91, 92], 'osen blizko', True, None]
+dlina_spiska = len(spisok)
+spisok.sort()
+print(dlina_spiska)
+print(spisok)
+# for i in range(0, dlina_spiska-1):
+#     if i != str:
+#         del spisok[i]
+# print(spisok)
