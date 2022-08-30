5361b1cff2ce700a812e5ece7d0320c5e79f98f2
Dvilkin <Dvilkin@bk.ru>
2022-08-30 03:19:09 +0300

    tak i ne pobedil(((

--- 6_4.py
+++ 6_4.py
@@ -2,10 +2,20 @@
 # образом, чтобы остались только строки, использование дополнительного списка незаконно
 
 spisok = [1, 2, 3, 'Leto', [89, 90, 91, 92], 'osen blizko', True, None]
-dlina_spiska = len(spisok)
-spisok.sort()
-print(dlina_spiska)
-print(spisok)
+rez = list(filter(lambda x: x.isalpha(), spisok))
+print(rez)
+
+
+# def filter_slov(n):
+#     if spisok[:] == str:
+#         print(spisok[:])
+#         return
+
+
+# dlina_spiska = len(spisok)
+# spisok.sort()
+# print(dlina_spiska)
+# print(spisok)
 # for i in range(0, dlina_spiska-1):
 #     if i != str:
 #         del spisok[i]
