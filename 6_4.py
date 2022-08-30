9467d12b12d18de23b035db40ca4659609ae5df3
Dvilkin <Dvilkin@bk.ru>
2022-08-30 10:16:33 +0300

    6_4 ok

--- 6_4.py
+++ 6_4.py
@@ -2,21 +2,5 @@
 # образом, чтобы остались только строки, использование дополнительного списка незаконно
 
 spisok = [1, 2, 3, 'Leto', [89, 90, 91, 92], 'osen blizko', True, None]
-rez = list(filter(lambda x: x.isalpha(), spisok))
+rez = list(filter(lambda x: type(x) == str, spisok))
 print(rez)
-
-
-# def filter_slov(n):
-#     if spisok[:] == str:
-#         print(spisok[:])
-#         return
-
-
-# dlina_spiska = len(spisok)
-# spisok.sort()
-# print(dlina_spiska)
-# print(spisok)
-# for i in range(0, dlina_spiska-1):
-#     if i != str:
-#         del spisok[i]
-# print(spisok)
