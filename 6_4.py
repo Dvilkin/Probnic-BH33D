1d32f78dd12f11caf0b44ccfd8c6fb98b6b5cd72
Dvilkin <Dvilkin@bk.ru>
2022-09-02 01:27:33 +0300

    6_4 ok

--- 6_4.py
+++ 6_4.py
@@ -2,5 +2,12 @@
 # образом, чтобы остались только строки, использование дополнительного списка незаконно
 
 spisok = [1, 2, 3, 'Leto', [89, 90, 91, 92], 'osen blizko', True, None]
-spisok = list(filter(lambda x: type(x) == str, spisok))
-print(spisok)
+
+
+def foo(spisok_strok):
+    spisok_strok = list(filter(lambda x: type(x) == str, spisok))
+    return spisok_strok
+
+
+spisok_strok = foo(input(f'Enter start:'))
+print(spisok_strok)
