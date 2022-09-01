1579a07ba116d0c2055db5fb8368e9c7e979cfae
Dvilkin <Dvilkin@bk.ru>
2022-09-01 03:03:40 +0300

    add for

--- 6_7.py
+++ 6_7.py
@@ -2,3 +2,15 @@
 # соседей, для крайних чисел одним из соседей является число с противоположной
 # стороны списка
 
+spisok_cisel = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 2, 1, 50, 100]
+len_spiska = len(spisok_cisel)
+print(type(len_spiska))
+print(len_spiska)
+print(spisok_cisel[0])
+for n in range(len(spisok_cisel)):
+        if n == 0:
+                print(spisok_cisel[n], spisok_cisel[-1] + spisok_cisel[n + 1])
+        elif n == len(spisok_cisel)-1:
+                print(spisok_cisel[n], spisok_cisel[n - 1] + spisok_cisel[0])
+        else:
+                print(spisok_cisel[n], spisok_cisel[n - 1] + spisok_cisel[n + 1])
