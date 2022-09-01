832fed1a76b5a15b3c0c4e366cfe14e271f9e4f7
Dvilkin <Dvilkin@bk.ru>
2022-09-01 04:36:05 +0300

    поиск по всему значению(

--- 6_8.py
+++ 6_8.py
@@ -2,8 +2,17 @@
 # поступает город, необходимо сказать из какой он страны
 
 slovar = {
-    "Belarus": ['Minsk', 'Brest', 'Vitebsk', 'Gomel', 'Grodno', 'Mogilev'],
-    "Russian Federation": ['Moscow', 'St. Petersburg', 'Sevastopol']
-
+    "Belarus": ['Minsk', 'Brest', 'Vitebsk', 'Gomel', 'Grodno', 'Mogilev', 1],
+    "Russian Federation": ['Moscow', 'St. Petersburg', 'Sevastopol'],
+    'rm': 2
 }
 print(slovar.values())
+
+
+def get_key(slovar, value):
+    for k, v in slovar.items():
+        if v == value:
+            return k
+
+
+print(get_key(slovar, ['Moscow', 'St. Petersburg', 'Sevastopol']))
