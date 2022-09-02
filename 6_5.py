eebc90f9fbefad3d2dee08261335c6bfcde55202
Dvilkin <Dvilkin@bk.ru>
2022-09-02 04:08:11 +0300

    6_5 net rehenia

--- 6_5.py
+++ 6_5.py
@@ -1,4 +1,15 @@
 # Дан список чисел, необходимо его развернуть без использования метода revese и
 # функции reversed, а так же дополнительного списка и среза
 
-list = [1, 2, 3, 50, 89, 21, 30, 8]
+my_range = [1, 2, 3, 50, 89, 21, 30, 8]
+print(type(my_range))
+
+
+def perevorot(ert):
+    for i in my_range:
+        my_range += my_range[-1]
+    return my_range
+
+
+my_range = perevorot(my_range)
+print(my_range)
