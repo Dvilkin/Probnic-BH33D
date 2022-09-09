74d8380929659324f2d490f1c1dc86e1be1b8f27
Dvilkin <Dvilkin@bk.ru>
2022-09-09 13:23:43 +0300

    add

--- Practic.py
+++ Practic.py
@@ -1,22 +1,12 @@
-# Вводится строка, вывести уникальные символы
-#
-# stroka = input("Enter stroky:")
-# for x in set(stroka):
-#     if stroka.count(x) == 1:
-#         print(x)
+import sqlite3
 
-# Вводится число, посчитать его факториал
 
-# chislo = int(input("Enter chislo:"))
-# factorial = 1
-# for x in range(1, chislo+1):
-#     factorial *=x
-#     print(factorial)
+conn = sqlite3.connect('db.db')
+cur = conn.cursor()
 
-#Составить программу дял вычисления среднего арифметического N произвольно вводимых чисел
-
-# n = int(input("Enter chislo:"))
-# s = 0
-# for i in range(n):
-#     s += 1
-# print(s/n)
+cur.execute('''
+CREATE TABLE IF NOT EXISTS categories(
+id INTEGER PRIMARY KEY AUTOINCREMENT,
+name VARCHAR(20) UNIQUE NOT NULL
+);
+''')
