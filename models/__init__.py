e958f0649ede265493581d1a278de3fbf6eaa536
Dvilkin <Dvilkin@bk.ru>
2022-09-13 12:40:00 +0300

    add import

--- models/__init__.py
+++ models/__init__.py
@@ -3,10 +3,10 @@ from typing import List
 from .models import Base, Category, Product, Order, OrderItem, User
 
 __all__: List[str] = [
-    'Base'
-    'Category'
-    'Product'
-    'Order'
-    'OrderItem'
+    'Base',
+    'Category',
+    'Product',
+    'Order',
+    'OrderItem',
     'User'
 ]
