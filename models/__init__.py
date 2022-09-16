4460f64348bbdf03fe7587ed4718344997044b34
Dvilkin <Dvilkin@bk.ru>
2022-09-16 12:08:04 +0300

    add

--- models/__init__.py
+++ models/__init__.py
@@ -1,7 +1,7 @@
 from typing import List
 
 from .models import Base, Category, Product, Order, OrderItem, User
-from .engine import DATABASE_URL, SYNC_ENGINE
+from .engine import DATABASE_URL, SYNC_ENGINE, create_sync_session
 
 __all__: List[str] = [
     'Base',
@@ -11,5 +11,6 @@ __all__: List[str] = [
     'OrderItem',
     'User',
     'DATABASE_URL',
-    'SYNC_ENGINE'
+    'SYNC_ENGINE',
+    'create_sync_session'
 ]
--- models/engine.py
+++ models/engine.py
@@ -1,4 +1,13 @@
 from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
 
 DATABASE_URL: str = 'postgresql://dvilkin:helhard@localhost:5432/bh33d'
 SYNC_ENGINE = create_engine(url=DATABASE_URL)
+Session = sessionmaker(biod=SYNC_ENGINE)
+
+def create_sync_session(func):
+    def wrapper(**kwargs):
+        with Session as session:
+            return func(**kwargs, session=session)
+    return wrapper
